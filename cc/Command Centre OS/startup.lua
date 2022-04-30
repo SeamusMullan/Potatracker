7d96571a98b55a3ef93b219ad5a14efef4192e82
Seamus Mullan <24carrotgaming@gmail.com>
2022-04-30 22:24:39 +0100

    bruh

--- cc/Command Centre OS/startup.lua
+++ cc/Command Centre OS/startup.lua
@@ -3,4 +3,6 @@ local monitors = { peripheral.find("monitor") }
 m = monitors[1]
 
 m.setTextScale(0.5)
-peripheral.call(side, "write", "Hello World!")
+peripheral.call(side, "write", "Sup Bitches! Ready to spy on some idiots?!")
+
+
