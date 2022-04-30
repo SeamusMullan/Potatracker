7794b884aad021cf5617a1212ebc20dc0c05539d
Seamus Mullan <24carrotgaming@gmail.com>
2022-04-30 20:01:07 +0100

    main phone os script with location and time

---
+++ phoneOS.lua
@@ -0,0 +1,23 @@
+-- get the current time and location
+local function get_time_and_location()
+    local time = os.date("%H:%M:%S")
+    local x, y, z = gps.locate(2)
+    if not x then
+        return time, "Unknown Location"
+    end
+    local location = string.format(x .. ", " .. y .. ", " .. z)
+    return time, location
+end
+
+
+
+
+while true do
+    local time, location = get_time_and_location()
+    local data = {
+        time = time,
+        location = location
+    }
+    print(data.time .. " : " .. data.location)
+    -- sleep for 1 second
+end
