9f62a16e6ef8213663ec9a0fa9da3a1b86a49adb
MajusDev <59688549+MajusDev@users.noreply.github.com>
2022-05-01 16:06:29 +0200

    removed stuff

--- cc/Phone OS/api/ui.lua
+++ cc/Phone OS/api/ui.lua
@@ -22,14 +22,4 @@ local function drawLogo()
     sleep(boot[i][2])
   end
 end
-function DrawUI()
-  term.clear()
-  term.setCursorPos(w/2-string.len(logo)/2, h/2)
-  term.setTextColor(colors.white)
-  print(logo)
-  term.setCursorPos(1, h-1)
-  print(taskbar[1])
-  term.setCursorPos(1, h)
-  print(taskbar[2])
-end
 drawLogo()
