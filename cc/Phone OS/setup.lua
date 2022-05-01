c56e201956a340917b72c02f59dbaeface6b209e
MajusDev <59688549+MajusDev@users.noreply.github.com>
2022-05-01 13:46:00 +0200

    fixed my dumdum

--- cc/Phone OS/setup.lua
+++ cc/Phone OS/setup.lua
@@ -1,8 +1,8 @@
 -- Installing Dependencies
 
-shell.run("wget https://raw.githubusercontent.com/SeamusMullan/Potatracker/main/cc/Phone%20OS/api/json.lua json")
-shell.run("wget https://raw.githubusercontent.com/SeamusMullan/Potatracker/main/cc/Phone%20OS/api/location.lua location")
-shell.run("wget https://raw.githubusercontent.com/SeamusMullan/Potatracker/main/cc/Phone%20OS/api/pinknet.lua pinknet")
-shell.run("wget https://raw.githubusercontent.com/SeamusMullan/Potatracker/main/cc/Phone%20OS/api/ui.lua ui")
+shell.run("wget https://raw.githubusercontent.com/SeamusMullan/Potatracker/main/cc/Phone%20OS/api/json.lua api/json")
+shell.run("wget https://raw.githubusercontent.com/SeamusMullan/Potatracker/main/cc/Phone%20OS/api/location.lua api/location")
+shell.run("wget https://raw.githubusercontent.com/SeamusMullan/Potatracker/main/cc/Phone%20OS/api/pinknet.lua api/pinknet")
+shell.run("wget https://raw.githubusercontent.com/SeamusMullan/Potatracker/main/cc/Phone%20OS/api/ui.lua api/ui")
 shell.run("wget https://raw.githubusercontent.com/SeamusMullan/Potatracker/main/cc/Phone%20OS/startup.lua startup")
 shell.run("reboot")
