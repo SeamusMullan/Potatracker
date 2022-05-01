242eb56f4793ca12d645c923dd84a24f9579e5fa
MajusDev <59688549+MajusDev@users.noreply.github.com>
2022-05-01 14:03:16 +0200

    fix retarded :flushed:

--- cc/Command Centre OS/setup.lua
+++ cc/Command Centre OS/setup.lua
@@ -1,6 +1,6 @@
 -- Installing Dependencies
 
-shell.run("wget https://raw.githubusercontent.com/SeamusMullan/Potatracker/main/cc/Command%20Cenre%20OS/api/json.lua api/json")
+shell.run("wget https://raw.githubusercontent.com/SeamusMullan/Potatracker/main/cc/Command%20Centre%20OS/api/json.lua api/json")
 
-shell.run("wget https://raw.githubusercontent.com/SeamusMullan/Potatracker/main/cc/Command%20Cenre%20OS/startup.lua startup")
+shell.run("wget https://raw.githubusercontent.com/SeamusMullan/Potatracker/main/cc/Command%20Centre%20OS/startup.lua startup")
 shell.run("reboot")
