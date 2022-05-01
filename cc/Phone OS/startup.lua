2c68d19b689de0f36d81eb0cd668746b6704fe2f
MajusDev <59688549+MajusDev@users.noreply.github.com>
2022-05-01 14:28:16 +0200

    sent to command centre

--- cc/Phone OS/startup.lua
+++ cc/Phone OS/startup.lua
@@ -14,7 +14,12 @@ local function potatoIcon()
 end
 
 while true do
+  pinknet.SendToCC()
   t, l = location.GetTimeAndLocation()
+  local loc = json.decode(l)
+  if loc == nil then
+    loc = "Unknown Location"
+  end
   term.clear()
   term.setCursorPos(1, 1)
   local id = pinknet.FindCommandCentre()
@@ -22,6 +27,6 @@ while true do
   term.setCursorPos(1, 2)
   term.write(string.format("Time: %s", t))
   term.setCursorPos(1, 3)
-  term.write(string.format("Location: %s", l))
+  term.write(string.format("Location: \n x:%s y:%s z:%s", loc.x, loc.y, loc.z))
   
 end
