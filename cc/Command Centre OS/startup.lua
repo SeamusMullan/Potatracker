944513866355582457d423f350178b7b009ae67e
MajusDev <59688549+MajusDev@users.noreply.github.com>
2022-05-01 13:56:50 +0200

    command centre shenanigans :flushed:

--- cc/Command Centre OS/startup.lua
+++ cc/Command Centre OS/startup.lua
@@ -13,5 +13,14 @@ m.setTextScale(0.5)
 m.write("Sup Bitches! Ready to spy on some idiots?!")
 
 while true do
-  
+  local id, message = rednet.receive()
+  if message == nil then
+    m.setCursorPos(1, 1)
+    m.clearLine()
+    m.write("No messages")
+  else
+    m.setCursorPos(1, 1)
+    m.clearLine()
+    m.write(string.format("Message from %s: %s", id, message))
+  end
 end
